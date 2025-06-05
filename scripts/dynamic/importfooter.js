76e5655fcb4d5cc3940d24609bd1e6ab5156719a
ISTEK <tamdacoder@gmail.com>
2025-06-04 22:33:02 -0500

    Update importfooter.js

--- scripts/dynamic/importfooter.js
+++ scripts/dynamic/importfooter.js
@@ -2,7 +2,7 @@ document.write(`
 <footer>
 <a class="footerlinks" href="https://github.com/BlookRig" target="_blank"><i class="fab fa-github"></i> GitHub</a>
 <a class="footerlinks" href="<!-- Not Yet -->" target="_blank"><i class="fab fa-discord"></i> Discord</a>
-<a class="footerlinks" href="<!-- Not Yet -->" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
+<a class="footerlinks" href="https://twitter.com/blookrig" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
 <a class="footerlinks" href="/donate.html"><i class="fas fa-heart"></i> Donate</a>
 </footer>
 `);
