6d94fdea4c4557f599ae41bb53a7e2f23a994d40
ISTEK <tamdacoder@gmail.com>
2025-06-04 21:43:37 -0500

    Create importfooter.js

---
+++ scripts/dynamic/importfooter.js
@@ -0,0 +1,8 @@
+document.write(`
+<footer>
+<a class="footerlinks" src="https://github.com/BlookRig" target="_blank"><i class="fab fa-github"></i></a>
+<a class="footerlinks" src="<!-- Not Yet -->" target="_blank"><i class="fab fa-discord"></i></a>
+<a class="footerlinks" src="<!-- Not Yet -->" target="_blank"><i class="fab fa-twitter"></i></a>
+<a class="footerlinks" src="/donate.html"><i class="fas fa-heart"></i></a>
+</footer>
+`);
