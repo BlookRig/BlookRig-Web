59c2e46da149c6f5932eb278afb787b992c9905a
ISTEK <tamdacoder@gmail.com>
2025-06-04 21:49:38 -0500

    shit mb

--- scripts/dynamic/importheader.js
+++ scripts/dynamic/importheader.js
@@ -1,12 +1,12 @@
 document.write(`
 <header>
 <div id="left" onclick="window.location.href='/';">
-<img class="logo" src="/img/favi.png" alt="BLKRIG">
+<img class="logo" href="/img/favi.png" alt="BLKRIG">
 </div>
 <div id="right">
-<a class="rightlinks" src="/products.html">Products</a>
-<a class="rightlinks" src="/credits.html">Credits</a>
-<a class="rightlinks" src="/versions.html">Versions</a>
-<a class="rightlinks" src="/docs">Docs</a>
+<a class="rightlinks" href="/products.html">Products</a>
+<a class="rightlinks" href="/credits.html">Credits</a>
+<a class="rightlinks" href="/versions.html">Versions</a>
+<a class="rightlinks" href="/docs">Docs</a>
 </div>
 </header>`);
