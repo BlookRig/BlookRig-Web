ef802a12d3276fafe9d4ae7558822835161cc57f
ISTEK <tamdacoder@gmail.com>
2025-06-04 21:25:11 -0500

    Create importheader.js

---
+++ scripts/dynamic/importheader.js
@@ -0,0 +1,11 @@
+document.write(`
+<header>
+<div id="left" onclick="window.location.href='/';">
+<img class="logo" src="/img/favi.png" alt="BLKRIG">
+</div>
+<div id="right">
+<a class="rightlinks" src="/products.html">Products</a>
+<a class="rightlinks" src="/versions.html">Versions</a>
+<a class="rightlinks" src="/docs">Docs</a>
+</div>
+</header>`);
