a5291b65f19edd9c3e1b2b9ddc39892ade038f96
ISTEK <tamdacoder@gmail.com>
2025-06-04 21:49:24 -0500

    shit mb

--- scripts/dynamic/importfooter.js
+++ scripts/dynamic/importfooter.js
@@ -1,8 +1,8 @@
 document.write(`
 <footer>
-<a class="footerlinks" src="https://github.com/BlookRig" target="_blank"><i class="fab fa-github"></i> GitHub</a>
-<a class="footerlinks" src="<!-- Not Yet -->" target="_blank"><i class="fab fa-discord"></i> Discord</a>
-<a class="footerlinks" src="<!-- Not Yet -->" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
-<a class="footerlinks" src="/donate.html"><i class="fas fa-heart"></i> Donate</a>
+<a class="footerlinks" href="https://github.com/BlookRig" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+<a class="footerlinks" href="<!-- Not Yet -->" target="_blank"><i class="fab fa-discord"></i> Discord</a>
+<a class="footerlinks" href="<!-- Not Yet -->" target="_blank"><i class="fab fa-twitter"></i> Twitter</a>
+<a class="footerlinks" href="/donate.html"><i class="fas fa-heart"></i> Donate</a>
 </footer>
 `);
