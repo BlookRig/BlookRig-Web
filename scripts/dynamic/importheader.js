533c041d00e68a8c0db580c2e3c13b05927feca1
ISTEK <tamdacoder@gmail.com>
2025-06-04 21:46:07 -0500

    Update importheader.js

--- scripts/dynamic/importheader.js
+++ scripts/dynamic/importheader.js
@@ -5,6 +5,7 @@ document.write(`
 </div>
 <div id="right">
 <a class="rightlinks" src="/products.html">Products</a>
+<a class="rightlinks" src="/credits.html">Credits</a>
 <a class="rightlinks" src="/versions.html">Versions</a>
 <a class="rightlinks" src="/docs">Docs</a>
 </div>
